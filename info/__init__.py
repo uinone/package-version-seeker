dd24ff059690665f4e5d3f61a8d06fff0bc1279d
uinone <tizm423@gmail.com>
2024-09-24 16:42:49 +0900

    [Add] Crawling pypi history

---
+++ info/__init__.py
@@ -0,0 +1,3 @@
+from .history import ReleaseHistory
+
+__all__ = ["ReleaseHistory"]
---
+++ info/history.py
@@ -0,0 +1,54 @@
+import requests
+from bs4 import BeautifulSoup
+
+from datetime import datetime
+
+class ReleaseHistory:
+    def __init__(self, project_name:str, release_history:dict):
+        self.project_name = project_name
+        self.version_to_datetime = release_history
+
+    def get_closest_version_by_datetime(self, target_datetime: datetime):
+        target_timestamp = target_datetime.timestamp()
+
+        minimum_diff = -1
+        closest_version = None
+        for k,v in self.version_to_datetime.items():
+            diff = abs(v["datetime"].timestamp() - target_timestamp)
+            if (closest_version is None) or (diff < minimum_diff):
+                minimum_diff = diff
+                closest_version = k
+        
+        return f"{self.project_name}=={closest_version}"
+    
+    def get_datetime_by_version(self, version:str):
+        try:
+            dt = self.version_to_datetime[version]
+            return dt
+        except KeyError:
+            print(f"version '{version}' does not exist in {self.project_name}'s release history\nCheck 'https://pypi.org/project/{self.project_name}/#history'")
+
+    def __repr__(self) -> str:
+        return f"[Project] {self.project_name}"
+
+    @classmethod
+    def get_project_release_history(cls, project_name:str):
+        release_history_url = f"https://pypi.org/project/{project_name}/#history"
+
+        release_history_response = requests.get(release_history_url)
+        soup = BeautifulSoup(release_history_response.text, "html.parser")
+
+        time_line = soup.find(name="div", attrs="release-timeline")
+        
+        releases = time_line.findAll(name="div", attrs="release")
+
+        release_history = {}
+        for release in releases:
+            version = release.find(name="p", attrs="release__version").text.strip().split("\n")[0]
+            dt = release.find(name="time").attrs["datetime"].split("+")[0]
+            release_history[version] = {"datetime": datetime.strptime(dt, "%Y-%m-%dT%H:%M:%S")}
+        
+        return ReleaseHistory(
+            project_name=project_name,
+            release_history=release_history
+        )
