d2d35c7a446b9f51bd86bf046907f302de571f5b
uinone <tizm423@gmail.com>
2024-10-07 01:57:40 +0900

    [Add] add project option and change parsing logic

--- src/info/module.py
+++ src/info/module.py
@@ -9,20 +9,21 @@ class ModuleType(Enum):
     VERSION_EMPTY_WITH_COMMENT = 5
 
 class Module:
-    def __init__(self, project_name:str, project_version:str, comment:str, module_type:ModuleType):
+    def __init__(self, project_name:str, project_option:str, project_version:str, comment:str, module_type:ModuleType):
         self.project_name = project_name
+        self.project_option = project_option
         self.project_version = project_version
         self.comment = comment
         self.module_type = module_type
     
     def __repr__(self) -> str:
         if self.module_type == ModuleType.NEWLINE:
-            return "\n"
+            return ""
 
         if self.module_type == ModuleType.JUST_COMMENT:
             return self.comment
 
-        repr_text = self.project_name + self.project_version if self.project_version is not None else ""
+        repr_text = self.project_name + (self.project_option if self.project_option is not None else "") + (self.project_version if self.project_version is not None else "")
         if self.module_type.name.endswith("WITH_COMMENT"):
             repr_text += self.comment
         
@@ -33,6 +34,7 @@ class Module:
         if len(content) == 0:
             return Module(
                 project_name=None,
+                project_option=None,
                 project_version=None,
                 comment=None,
                 module_type=ModuleType.NEWLINE
@@ -41,6 +43,7 @@ class Module:
         if content[0] == "#":
             return Module(
                 project_name=None,
+                project_option=None,
                 project_version=None,
                 comment=content,
                 module_type=ModuleType.JUST_COMMENT
@@ -49,6 +52,7 @@ class Module:
         content = content.strip()
 
         project_name = None
+        project_option = None
         project_version = None
         module_type = None
         comment = None
@@ -56,6 +60,15 @@ class Module:
         # Check comment existence
         if content.find("#") != -1:
             comment_start_idx = content.find("#")
+
+            # Check additional spaces
+            temp_idx = comment_start_idx-1
+            while content[temp_idx] == " ":
+                temp_idx -= 1
+            temp_idx += 1
+
+            comment_start_idx = temp_idx
+
             comment = content[comment_start_idx:]
             content = content[:comment_start_idx]
             content = content.strip()
@@ -86,10 +99,15 @@ class Module:
                 module_type = ModuleType.VERSION_EXIST_WITH_COMMENT
             
             project_name = content[:postive_index].strip()
-            project_version = content[postive_index:]
+            project_version = "".join(content[postive_index:].split(" ")) # Trim some spaces around project version
+            
+        if project_name.find("[") != -1:
+            project_option = project_name[project_name.find("["):]
+            project_name = project_name[:project_name.find("[")]
             
         return Module(
             project_name=project_name,
+            project_option=project_option,
             project_version=project_version,
             comment=comment,
             module_type=module_type
