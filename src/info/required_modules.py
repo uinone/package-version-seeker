7a007140193d21536ccea360cfa47767d944aa00
uinone <tizm423@gmail.com>
2024-10-07 02:13:16 +0900

    [Change] Change RequireModules class due to adding Module class #5

--- src/info/required_modules.py
+++ src/info/required_modules.py
@@ -1,31 +1,28 @@
 from .history import ReleaseHistory
+from .module import Module, ModuleType
 
 from typing import Optional
 
 class RequiredModules:
-    def __init__(self, infos:dict):
+    def __init__(self, infos:list[Module]):
         self.infos = infos
 
     def __repr__(self) -> str:
         repr_text = ""
 
-        for k, v in self.infos.items():
-            if v is None:
-                repr_text += k
-                repr_text += "\n"
-            else:
-                repr_text += k + v
-                repr_text += "\n"
+        for info in self.infos:
+            print(info.__repr__())
+            repr_text += info.__repr__() + "\n"
         
         return repr_text[:-1]
     
     def _fill_empty_versions(self, target_project:str, target_project_version:str):
-        module_with_empty_version = {}
+        module_with_empty_version = []
 
         # Aggregating
-        for k, v in self.infos.items():
-            if v is None:
-                module_with_empty_version[k] = None
+        for idx, info in enumerate(self.infos):
+            if (info.module_type == ModuleType.VERSION_EMPTY) or (info.module_type == ModuleType.VERSION_EMPTY_WITH_COMMENT):
+                module_with_empty_version.append(idx)
         
         target_project_release_hist = ReleaseHistory.get_project_release_history(target_project)
         target_datetime = target_project_release_hist.get_datetime_by_version(target_project_version)
@@ -35,12 +32,11 @@ class RequiredModules:
 
         target_datetime = target_datetime["datetime"]
 
-        for module_name in module_with_empty_version.keys():
+        for idx in module_with_empty_version:
+            module_name = self.infos[idx].project_name
             release_hist = ReleaseHistory.get_project_release_history(module_name)
             closest_version = release_hist.get_closest_version_by_datetime(target_datetime)
-            module_with_empty_version[module_name] = closest_version
-        
-        self.infos.update(module_with_empty_version)
+            self.infos[idx].project_version = closest_version
 
         return self
     
@@ -48,29 +44,15 @@ class RequiredModules:
         with open(path, "w") as f:
             f.write(self.__repr__())
 
-
     @classmethod
     def get_requirement_text(cls, path:str, target_project:str, target_project_version:str) -> Optional[ReleaseHistory]:
-        required_module_infos = {}
+        required_modules:list[Module] = []
+
         try:
             with open(path, "r") as f:
-                required_module_names = [module_name for module_name in f.read().split("\n") if not(module_name.startswith("#")) and len(module_name) > 0]
-                
-                for module_name in required_module_names:
-                    split_prefix_candidate = [module_name.find("<"), module_name.find("="), module_name.find(">")]
-                    split_prefix_candidate.sort()
-
-                    postive_index = -1
-                    for cand in split_prefix_candidate:
-                        if cand > 0:
-                            postive_index = cand
-                            break
-
-                    if postive_index == -1:
-                        required_module_infos[str(module_name).strip()] = None
-                    else:
-                        required_module_infos[str(module_name[:postive_index]).strip()] = module_name[postive_index:]
+                for line in f.read().split("\n"):
+                    required_modules.append(Module.parse_content(line))
             
-            return RequiredModules(infos=required_module_infos)._fill_empty_versions(target_project, target_project_version)
+            return RequiredModules(infos=required_modules)._fill_empty_versions(target_project, target_project_version)
         except Exception as e:
             print(f"[FAILED]\n{e}")
